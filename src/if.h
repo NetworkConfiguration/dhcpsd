555994c884c4a8bb0e58caa8a732be7509f967d9
Roy Marples <roy@marples.name>
2026-05-24 21:54:37 +0000

    Add test for NetBSD-9

--- src/if.h
+++ src/if.h
@@ -50,9 +50,8 @@ struct ctx;
 struct dhcp_pool;
 struct iovec;
 
-#if defined(__NetBSD_Version__) && __NetBSD_Version__ <= 1000000000
-#undef if_mtu
-#endif
+/* Fix compile on NetBSD-9 */
+#include "config.h"
 
 struct interface {
 	TAILQ_ENTRY(interface) if_next;
